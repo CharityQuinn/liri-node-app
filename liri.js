8e3d771427fa9051399ece33bca8f9d86a8fbda5
CharityQuinn <charityaquinn@gmail.com>
2019-01-19 12:10:07 -0500

    Added the switch case code to liri.js, trying to get something working

--- liri.js
+++ liri.js
@@ -1,70 +1,95 @@
-require(".env").config();
+require("./.env").config();
+console.log('this is loaded');
+
+var axios = require("axios");
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+var axios = require("axios");
+
+
+
 
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
 
-// you should be able to take in these commands:
-//  `concert-this`
+// Load the fs package to read and write
+var fs = require("fs");
 
-// * `spotify-this-song`
+// Take two arguments.
+// The first will be the action (i.e. "concert-this", "spotify-this-song", etc.)
+// The second will be the amount that will be added, spotify-this-songn, etc.
+var action = process.argv[2];
+var value = process.argv[3];
 
-// * `movie-this`
+// We will then create a switch-case statement (if-else would also work).
+// The switch-case will direct which function gets run.
+switch (action) {
+  case "concert-this":
+    concertThis();
+    break;
 
-// * `do-what-it-says
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+}
+
+
+
+
+
 
 // each command should be like this
 //node liri.js concert-this <artist/I Want it That Way;
 
-fs.readFile("random.txt", "text", function(err, data) {
-  if (err) {
-    return console.log(err);
-  }
-  var output = data.split(",");
+function concertThis() {
+  var bandsInTnKey = "7e9d74149cb19a07ef1d023000b73376"
+}
 
-  // Loop Through the newly created output array
-  for (var i = 0; i < output.length; i++) {
-    // Print each element (item) of the array/
-    console.log(output[i]);
-  }
-});
+function spotifyThisSong() {
+  // if no song play "The Sign" by Ace of Base
+  fs.readFile("random.txt", "track", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var output = data.split(",");
 
 
-// var googleBooksAPI = "AIzaSyCheolDq79sZudYdTX1G6FspSWLpQXDEiI";
-//     var googleBooksURL = "https://www.googleapis.com/books/v1/volumes?q=" + pastBooks[0].title + pastBooks[0].author + "&key=" + googleBooksAPI;
+  });
 
-function({type: 'track', query: 'I Want it That Way', limit: 20}, callback);
-
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: 6e00397819554dfe8ccad8e00e774755,
-  secret: ef55a913c08d4feea07d388416fde3ae
-});
- 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(data); 
-});
-var spotUrl = "spotify:track:6rqhFgbbKwnb9MLmUQDhG6"
-
-//</your></your>spotifiy id = 
-
-//spotify secret = ef55a913c08d4feea07d388416fde3ae
-
-`node liri.js movie-this '<movie name here>'`
-
+}
+//`node liri.js movie-this '<movie name here>'`
 // that gives you:  * Title of the movie.
-      //  * Year the movie came out.
-      //  * IMDB Rating of the movie.
-      //  * Rotten Tomatoes Rating of the movie.
-      //  * Country where the movie was produced.
-      //  * Language of the movie.
-      //  * Plot of the movie.
-      //  * Actors in the movie.
+//  * Year the movie came out.
+//  * IMDB Rating of the movie.
+//  * Rotten Tomatoes Rating of the movie.
+//  * Country where the movie was produced.
+//  * Language of the movie.
+//  * Plot of the movie.
+//  * Actors in the movie.
 
-      // default move is Mr. Nobody
-// if no song play "The Sign" by Ace of Base
+// default move is Mr. Nobody
+function movieThis() {
+  var omdbKey = "8c2420ae"
+  var omdbApi = " http://www.omdbapi.com/?i=tt3896198&apikey=8c2420ae"
+
+  // Then run a request with axios to the OMDB API with the movie specified
+  axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+      console.log("The movie's rating is: " + response.data.imdbRating);
+    }
+  );
+}
+
+function doWhatItSays() {
+
+}
